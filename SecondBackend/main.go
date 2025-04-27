2ab4ab82460b79adda8118edc61e2aeb656f6f40
LegendaryLoona <loonalegendary@gmail.com>
2025-04-27 21:01:04 +0200

    Go cors

--- SecondBackend/main.go
+++ SecondBackend/main.go
@@ -3,20 +3,25 @@ package main
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 
+	// Add CORS config
+	r.Use(cors.Default())
+
 	r.GET("/profile", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"first_name": "John",
 			"last_name":  "Doe",
-			"email":      "john.doe@example.com",
+			"email":      "john@example.com",
 			"position":   "Mobile Developer",
 		})
 	})
 
-	r.Run()
+	port := "8080" // or get from environment on Render
+	r.Run(":" + port)
 }
